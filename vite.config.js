df7fb4f11a162e14c251eee2d943c0c9321fe15e
Scott Shetley <davidscottshetley@gmail.com>
2025-08-18 09:05:41 -0400

    PWA ability

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig ({
-  plugins: [react ()],
+  plugins: [
+    react (),
+    VitePWA ({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'RepForge',
+        short_name: 'RepForge',
+        description: 'A workout tracker for the StrongLifts 5x5 program.',
+        theme_color: '#1F2937', // A dark theme color
+        background_color: '#111827',
+        icons: [
+          {
+            src: 'icons/192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
